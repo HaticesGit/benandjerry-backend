0bd6e950313432bedcb4af9759872486da0e0a76
HaticesGit <hatice.baldemir@hotmail.com>
2026-06-18 15:55:35 +0200

    feat: add mongodb connection

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-const express = require("express");
+require("dotenv").config();
 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "API works" });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
