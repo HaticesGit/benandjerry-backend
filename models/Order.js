d4f304ae31dd8e49cdd42c822649a6f4e72be616
HaticesGit <hatice.baldemir@hotmail.com>
2026-06-22 10:41:05 +0200

    refactor: replace size with cupOrCone

--- models/Order.js
+++ models/Order.js
@@ -2,18 +2,37 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema(
   {
-    customerName: { type: String, required: true },
-    address: { type: String, required: true },
-    flavor: { type: String, required: true },
-    topping: { type: String, required: true },
-    size: { type: String, required: true },
-    totalPrice: { type: Number, required: true },
+    customerName: { 
+      type: String, 
+      required: true },
+
+    address: { 
+      type: String, 
+      required: true },
+
+    flavor: { 
+      type: String, 
+      required: true },
+
+    topping: { 
+      type: String, 
+      required: true },
+
+    cupOrCone: { 
+      type: String, 
+      required: true },
+
+    totalPrice: { 
+      type: Number, 
+      required: true },
+
     status: {
       type: String,
       default: "te verwerken",
       enum: ["te verwerken", "verzonden", "geannuleerd"],
     },
   },
+  
   { timestamps: true }
 );
 
